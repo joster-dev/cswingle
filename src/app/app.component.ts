1a39a157ba72c57b26d58c7422220dfc97a23c25
Joster422 <Joster422@gmail.com>
2018-07-03 00:02:10 -0400

    feat: icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'cs-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "cs-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'cs';
-}
+export class AppComponent {}
