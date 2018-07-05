8490e37d0908aca4cc48992173fef010b9a72e88
Joster422 <Joster422@gmail.com>
2018-07-04 20:04:34 -0400

    feat: image click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,59 @@
-import { Component } from "@angular/core";
+import { Component, HostListener } from "@angular/core";
 
 @Component({
   selector: "cs-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {}
+export class AppComponent {
+  isFullscreen = false;
+
+  people: any[] = [
+    new Image("https://i.imgur.com/m3i5PjR.jpg"),
+    new Image("https://i.imgur.com/wfr3o0D.jpg"),
+    new Image("https://i.imgur.com/zEsciF4.jpg"),
+    new Image("https://i.imgur.com/VjHHGqi.jpg"),
+    new Image("https://i.imgur.com/9p8QDgL.jpg")
+  ];
+
+  clay: Image[] = [
+    new Image("https://i.imgur.com/diuNWsH.jpg"),
+    new Image("https://i.imgur.com/L5iHsNu.jpg"),
+    new Image("https://i.imgur.com/RD1hIwp.jpg"),
+    new Image("https://i.imgur.com/opcU9Su.jpg"),
+    new Image("https://i.imgur.com/wQg2k43.png")
+  ];
+
+  clickImg(image: Image) {
+    if (this.isFullscreen === false) {
+      setTimeout(() => {
+        this.isFullscreen = true;
+        image.isFullscreen = true;
+      }, 1);
+    }
+  }
+
+  @HostListener("window:click")
+  windowClick() {
+    if (this.isFullscreen === true) {
+      this.disableFullscreen();
+    }
+  }
+
+  private disableFullscreen() {
+    this.isFullscreen = false;
+    this.people = this.people.map(item => {
+      item.isFullscreen = false;
+      return item;
+    });
+    this.clay = this.clay.map(item => {
+      item.isFullscreen = false;
+      return item;
+    });
+  }
+}
+
+class Image {
+  isFullscreen = false;
+  constructor(private src: string) {}
+}
